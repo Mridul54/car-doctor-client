780076c68ed55bf217f1bee5ecc71843927c7ee1
Mridul54 <dasmridul364@gmail.com>
2023-10-30 17:14:14 +0600

    adding services

--- src/Pages/Shared/NavBar/NavBar.jsx
+++ src/Pages/Shared/NavBar/NavBar.jsx
@@ -1,12 +1,29 @@
 import { Link } from "react-router-dom";
 import logo from "../../../assets/logo.svg"
+import { useContext } from "react";
+import { AuthContext } from "../../../providers/AuthProvider";
 
 const NavBar = () => {
+
+    const {user, logOut} = useContext(AuthContext);
+
+    const handleLogOut = () => {
+        logOut()
+        .then(() => {})
+        .catch(error => console.log(error))
+    }
+
+
     const navItems = <>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/signup">Registration</Link></li>
+        
+        {user?.email ? <>
+            <li><Link to="/bookings">My bookings</Link></li>
+            <li><button onClick={handleLogOut}>LogOut</button></li>
+        </>
+        : <li><Link to="/login">Login</Link></li>}
+        
 
     </>
     return (
